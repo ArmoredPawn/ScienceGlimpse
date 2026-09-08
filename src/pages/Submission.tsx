125e0d9113c2c15496d548f568a46486d3cfa7a8
Armoredpawn <ayush25srivastava25@gmail.com>
2026-09-08 17:27:09 -0400

    Fixed firestore

--- src/pages/Submission.tsx
+++ src/pages/Submission.tsx
@@ -44,7 +44,7 @@ const Submission = () => {
             <Button 
               size="lg" 
               className="px-8 py-3"
-              onClick={() => window.open('https://forms.gle/sTmMboMnpGJeJPNHA', '_blank')}
+              onClick={() => window.open('https://forms.gle/sTmMboMnpGJeJPNHA', '_blank', 'noopener,noreferrer')}
             >
               <ExternalLink className="w-5 h-5 mr-2" />
               Submit Your Article
